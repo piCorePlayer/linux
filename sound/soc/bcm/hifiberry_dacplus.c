c29a34178c7ba332c9ac360b317063e6040922c9
DigitalDreamtime <clive.messer@digitaldreamtime.co.uk>
2016-05-06 22:22:39 +0100

    384k: hifiberry_dacplus: Support 352k8 sample rate
    Use CLK44EN for 352k8 sample rate support.
    
    Set (initial) the clk rate to CLK_48EN_RATE in the init method.
    Set (reset) the clk rate to CLK_48EN_RATE in the shutdown method.
    (Otherwise codec pcm512x_dai_startup_master method can call
     snd_pcm_hw_constraint_ratnums based on CLK_44EN, which will
     result in loss of 384k sample rate, even though we are
     capable of it with CLK_48EN, because 22579200/64=352800.)
    
    Signed-off-by: DigitalDreamtime <clive.messer@digitaldreamtime.co.uk>
    
    # Conflicts:
    #	sound/soc/bcm/hifiberry_dacplus.c
    
    # Conflicts:
    #	sound/soc/bcm/hifiberry_dacplus.c
    
    # Conflicts:
    #	sound/soc/bcm/hifiberry_dacplus.c

--- sound/soc/bcm/hifiberry_dacplus.c
+++ sound/soc/bcm/hifiberry_dacplus.c
@@ -185,7 +185,7 @@ static void snd_rpi_hifiberry_dacplus_set_sclk(struct snd_soc_component *compone
 static int snd_rpi_hifiberry_dacplus_init(struct snd_soc_pcm_runtime *rtd)
 {
 	struct snd_soc_component *component = asoc_rtd_to_codec(rtd, 0)->component;
-	struct pcm512x_priv *priv;
+	struct pcm512x_priv *priv = snd_soc_codec_get_drvdata(codec);
 	struct snd_soc_card *card = &snd_rpi_hifiberry_dacplus;
 
 	if (slave)
@@ -205,8 +205,16 @@ static int snd_rpi_hifiberry_dacplus_init(struct snd_soc_pcm_runtime *rtd)
 		snd_soc_component_update_bits(component, PCM512x_BCLK_LRCLK_CFG, 0x31, 0x11);
 		snd_soc_component_update_bits(component, PCM512x_MASTER_MODE, 0x03, 0x03);
 		snd_soc_component_update_bits(component, PCM512x_MASTER_CLKDIV_2, 0x7f, 63);
+
+		/*
+		 * Default sclk to CLK_48EN_RATE, otherwise codec
+		 *  pcm512x_dai_startup_master method could call
+		 *  snd_pcm_hw_constraint_ratnums using CLK_44EN/64
+		 *  which will mask 384k sample rate.
+		 */
+		if (!IS_ERR(priv->sclk))
+			clk_set_rate(priv->sclk, CLK_48EN_RATE);
 	} else {
-		priv = snd_soc_component_get_drvdata(component);
 		priv->sclk = ERR_PTR(-ENOENT);
 	}
 
@@ -323,6 +331,18 @@ static void snd_rpi_hifiberry_dacplus_shutdown(
 	snd_soc_component_update_bits(component, PCM512x_GPIO_CONTROL_1, 0x08, 0x00);
 	if (auto_mute)
 		gpiod_set_value_cansleep(snd_mute_gpio, 1);
+
+	if (snd_rpi_hifiberry_is_dacpro) {
+		struct pcm512x_priv *priv = snd_soc_codec_get_drvdata(codec);
+		/*
+		 * Default sclk to CLK_48EN_RATE, otherwise codec
+		 *  pcm512x_dai_startup_master method could call
+		 *  snd_pcm_hw_constraint_ratnums using CLK_44EN/64
+		 *  which will mask 384k sample rate.
+		 */
+		if (!IS_ERR(priv->sclk))
+			clk_set_rate(priv->sclk, CLK_48EN_RATE);
+	}
 }
 
 /* machine stream operations */
