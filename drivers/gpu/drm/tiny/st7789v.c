0dce26db605ac14388fca3580067ba9e84d86cb4
paul-1 <6473457+paul-1@users.noreply.github.com>
2025-03-28 20:10:29 -0400

    st7789v: Use fbdev-dma

--- drivers/gpu/drm/tiny/st7789v.c
+++ drivers/gpu/drm/tiny/st7789v.c
@@ -24,7 +24,7 @@
 #include <drm/drm_drv.h>
 #include <drm/drm_fb_dma_helper.h>
 #include <drm/drm_fb_helper.h>
-#include <drm/drm_fbdev_generic.h>
+#include <drm/drm_fbdev_dma.h>
 #include <drm/drm_fourcc.h>
 #include <drm/drm_framebuffer.h>
 #include <drm/drm_gem_atomic_helper.h>
@@ -368,7 +368,7 @@ static int st7789v_probe(struct spi_device *spi)
 
 	spi_set_drvdata(spi, drm);
 
-	drm_fbdev_generic_setup(drm, 0);
+	drm_fbdev_dma_setup(drm, 0);
 
 	return 0;
 }
